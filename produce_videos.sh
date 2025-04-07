f7b5e1ed4356731e0ae9f7702b37136ec69aa8da
Adam Koziak <adam.koziak2@gmail.com>
2025-04-07 00:12:00 -0400

    initial commit

---
+++ produce_videos.sh
@@ -0,0 +1,8 @@
+python SurRoL/surrol/data/data_generation.py --env GauzeRetrieve-v0 --video
+python SurRoL/surrol/data/data_generation.py --env PegTransfer-v0 --video
+python SurRoL/surrol/data/data_generation.py --env NeedleRegrasp-v0 --video
+python SurRoL/surrol/data/data_generation.py --env BiPegTransfer-v0 --video
+python SurRoL/surrol/data/data_generation.py --env ECMReach-v0 --video
+python SurRoL/surrol/data/data_generation.py --env MisOrient-v0 --video
+python SurRoL/surrol/data/data_generation.py --env StaticTrack-v0 --video
+python SurRoL/surrol/data/data_generation.py --env ActiveTrack-v0 --video
